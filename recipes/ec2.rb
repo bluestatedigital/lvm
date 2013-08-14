a71a3b3f082f377aa03d67b051f664ecac703037
Joel Barciauskas <jbarciauskas@bluestatedigital.com>
2013-08-14 18:22:15 -0400

    Only do this if the platform is amazon

--- recipes/ec2.rb
+++ recipes/ec2.rb
@@ -1,9 +1,11 @@
 include_recipe "lvm::default"
 if(node[:ec2][:instance_type] == 'm1.xlarge')
   # First device comes mounted
-  mount "/dev/sdb" do
-    device "/dev/sdb"
-    action :umount
+  if(node[:platform] == 'amazon')
+    mount "/dev/sdb" do
+      device "/dev/sdb"
+      action :umount
+    end
   end
   lvm_volume_group 'vg00' do
     physical_volumes [ '/dev/sdb', '/dev/sdc', '/dev/sdd', '/dev/sde' ]
