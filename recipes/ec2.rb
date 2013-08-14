4bd3618b562fe392fe6977713d1d88f5914be2c5
Joel Barciauskas <jbarciauskas@bluestatedigital.com>
2013-08-14 18:05:55 -0400

    Create ec2 LVM recipe to automatically stripe volumes based on instance type

---
+++ recipes/ec2.rb
@@ -0,0 +1,12 @@
+include_recipe "lvm::default"
+if(node[:ec2][:instance_type] == 'm1.xlarge') do
+  lvm_volume_group 'vg00' do
+    physical_volumes [ /dev/sdb, /dev/sdb, /dev/sdc, /dev/sdd ]
+    logical_volume 'data1' do
+      size '75%VG'
+      filesystem 'ext4'
+      mount_point :location => '/media/ephemeral0', :options => 'noatime,nodiratime'
+      stripes 4
+    end
+  end
+end
