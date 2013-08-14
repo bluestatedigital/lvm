463d312bd8f70c9861a33150eb16255c5ba9c411
Joel Barciauskas <jbarciauskas@bluestatedigital.com>
2013-08-14 18:19:46 -0400

    Unmount /dev/sdb first

--- recipes/ec2.rb
+++ recipes/ec2.rb
@@ -1,5 +1,10 @@
 include_recipe "lvm::default"
 if(node[:ec2][:instance_type] == 'm1.xlarge')
+  # First device comes mounted
+  mount "/dev/sdb" do
+    device "/dev/sdb"
+    action :umount
+  end
   lvm_volume_group 'vg00' do
     physical_volumes [ '/dev/sdb', '/dev/sdc', '/dev/sdd', '/dev/sde' ]
     logical_volume 'data1' do
