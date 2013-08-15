8adce95a9360505918c0b58c5d67f104c46bb8d1
Joel Barciauskas <jbarciauskas@bluestatedigital.com>
2013-08-15 11:46:40 -0400

    Use bash script to force unmounting

--- recipes/ec2.rb
+++ recipes/ec2.rb
@@ -1,5 +1,18 @@
 include_recipe "lvm::default"
 if(node[:ec2][:instance_type] == 'm1.xlarge')
+  #
+  # Unmount the Auto-mounted ephemerial drive
+  # Chef's mount resource seems to balk at this, so we're scripting it
+  #
+   
+  bash "unmount_default_ephemeral" do
+    code <<-EOH
+      umount /media/ephemeral0
+    EOH
+    user "root"
+    cwd "/tmp"
+    not_if "lvs| grep data1"
+  end
   # First device comes mounted
   lvm_volume_group 'vg00' do
     physical_volumes [ '/dev/xvdb', '/dev/xvdc', '/dev/xvdd', '/dev/xvde' ]
