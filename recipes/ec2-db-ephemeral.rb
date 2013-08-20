ce8489d2df1a60491c49d78cfa2a7cc6aec62719
Josiah C Webb <rootkix@gmail.com>
2013-08-20 16:28:52 -0400

    trying this again post bsd github repo access magic..

--- recipes/ec2-db-ephemeral.rb
+++ recipes/ec2-db-ephemeral.rb
@@ -1,15 +1,16 @@
 include_recipe "lvm::default"
-if(node[:ec2][:instance_type] == 'm1.xlarge')
+
+if(node[:ec2][:instance_type] =~ /(cc2\.8|m1\.)xlarge/)
   #
   # Unmount the Auto-mounted ephemerial drive
   # Chef's mount resource seems to balk at this, so we're scripting it
   #
-   
   if(node[:platform] == 'centos')
     mount "/dev/xvdb" do
       device "/dev/xvdb"
       action [:umount, :disable]
-    end 
+    end
+
     bash "unmount_default_ephemeral" do
       code <<-EOH
         umount /mnt/ephemeral
@@ -19,12 +20,13 @@ if(node[:ec2][:instance_type] == 'm1.xlarge')
       only_if "df | grep xvdb"
     end
   end
-  # First device comes mounted
+
   if(node[:platform] == 'amazon')
     mount "/dev/sdb" do
       device "/dev/sdb"
       action [:umount, :disable]
-    end 
+    end
+
     bash "unmount_default_ephemeral" do
       code <<-EOH
         umount /media/ephemeral0
@@ -34,8 +36,8 @@ if(node[:ec2][:instance_type] == 'm1.xlarge')
       only_if "df | grep sdb"
     end
   end
-  # First device comes mounted
-  lvm_volume_group 'vg00' do
+
+  lvm_volume_group 'vg0' do
     physical_volumes [ '/dev/xvdb', '/dev/xvdc', '/dev/xvdd', '/dev/xvde' ]
     logical_volume 'data1' do
       size '75%VG'
