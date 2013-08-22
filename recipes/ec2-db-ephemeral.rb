e719c725f4acbf27af4a39ba693dfa42912a0cd7
Josiah C Webb <rootkix@gmail.com>
2013-08-22 10:11:20 -0400

    added logic to lvm ec2 recipe that allows us to put it in the ec2-dbserver chef role without it trying to setup LVM stuff for pod's that are already built and LVM'd, etc..

--- recipes/ec2-db-ephemeral.rb
+++ recipes/ec2-db-ephemeral.rb
@@ -1,54 +1,45 @@
 # Cookbook Name:: lvm
-# Recipe:: ec2-clusterdb-lvm-ephemeral
+# Recipe:: ec2-db-ephemeral
 
 include_recipe 'lvm'
-
-pod_std = '/dev/mapper/vg0-data1'
-pod_bak = '/dev/mapper/vgbackup-lvbackup'
 devs = ['/dev/xvdb', '/dev/xvdc', '/dev/xvdd', '/dev/xvde'].sort
 
-begin
-  # already a standard db pod
-  return if node[:filesystem][pod_std]
-rescue ArgumentError
-  begin
-    # already a backup db pod
-    return if node[:filesystem][pod_bak]
-  rescue ArgumentError
+# do nothing if volume group already present
+unless %x{df} =~ /vg(0|backup)-(data1|lvbackup)/
+
+  mount "disable_default_ephemeral" do
+    action :disable
+    device "/dev/xvdb"
   end
-end
 
-mount "disable_default_ephemeral" do
-  action :disable
-  device "/dev/xvdb"
-end
-
-bash "unmount_default_ephemeral" do
-  code <<-EOH
-    umount /dev/xvdb
-  EOH
-  user "root"
-  cwd "/tmp"
-  only_if "df | grep xvdb"
-end
-
-lvm_volume_group 'vg0' do
-  physical_volumes devs
-  logical_volume 'data1' do
-    size '85%VG'
-    filesystem 'ext4'
-    mount_point :location => '/mnt/ephemeral', :options => 'noatime,nodiratime'
-    stripes devs.count
+  bash "unmount_default_ephemeral" do
+    code <<-EOH
+      umount /dev/xvdb
+    EOH
+    user "root"
+    cwd "/tmp"
+    only_if "df | grep xvdb"
   end
-end
 
-bash "mount_lvm_ephemeral" do
-  code <<-EOH
-    mount -t ext4 -O rw,noatime,nodiratime /dev/mapper/vg0-data1 /mnt/ephemeral
-  EOH
-  user "root"
-  cwd "/tmp"
-  not_if "df | grep vg0-data1"
-end
+  lvm_volume_group 'vg0' do
+    physical_volumes devs
+    logical_volume 'data1' do
+      size '85%VG'
+      filesystem 'ext4'
+      # rightscale uses '/mnt/ephemeral'
+      mount_point :location => '/mnt/ephemeral', :options => 'noatime,nodiratime'
+      stripes devs.count
+    end
+  end
 
+  bash "mount_lvm_ephemeral" do
+    code <<-EOH
+      mount -t ext4 -O rw,noatime,nodiratime /dev/mapper/vg0-data1 /mnt/ephemeral
+    EOH
+    user "root"
+    cwd "/tmp"
+    not_if "df | grep vg0-data1"
+  end
+
+end
 
