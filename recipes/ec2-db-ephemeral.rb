d71b5f282f949435a2bee7c8e168c036193c9f83
Josiah C Webb <rootkix@gmail.com>
2013-08-22 11:40:54 -0400

    adding back the mount resource with the correct mount point :disable action

--- recipes/ec2-db-ephemeral.rb
+++ recipes/ec2-db-ephemeral.rb
@@ -1,22 +1,24 @@
 # Cookbook Name:: lvm
 # Recipe:: ec2-db-ephemeral
 
-# chef's mount resource does not work consistently
-
 include_recipe 'lvm'
 devs = ['/dev/xvdb', '/dev/xvdc', '/dev/xvdd', '/dev/xvde'].sort
 
-# do nothing if volume group already present
+# skip recipe if VG is already present
 unless %x{df} =~ /vg(0|backup)-(data1|lvbackup)/
 
-  bash "unmount_default_ephemeral" do
+  bash 'unmount_default_ephemeral' do
     code <<-EOH
       umount /dev/xvdb
-      sed -i '/\dev\/\\(xv\\|s\\)db/d' /etc/fstab
     EOH
-    user "root"
-    cwd "/tmp"
-    only_if "df | grep xvdb"
+    user 'root'
+    cwd '/tmp'
+    only_if 'df | grep xvdb'
+  end
+
+  mount '/mnt/ephemeral' do
+    device '/dev/xvdb'
+    action :disable
   end
 
   lvm_volume_group 'vg0' do
@@ -24,19 +26,18 @@ unless %x{df} =~ /vg(0|backup)-(data1|lvbackup)/
     logical_volume 'data1' do
       size '85%VG'
       filesystem 'ext4'
-      # rightscale uses '/mnt/ephemeral'
       mount_point :location => '/mnt/ephemeral', :options => 'noatime,nodiratime'
       stripes devs.count
     end
   end
 
-  bash "mount_lvm_ephemeral" do
+  bash 'mount_lvm_ephemeral' do
     code <<-EOH
       mount -t ext4 -O rw,noatime,nodiratime /dev/mapper/vg0-data1 /mnt/ephemeral
     EOH
-    user "root"
-    cwd "/tmp"
-    not_if "df | grep vg0-data1"
+    user 'root'
+    cwd '/tmp'
+    not_if 'df | grep vg0-data1'
   end
 
 end
