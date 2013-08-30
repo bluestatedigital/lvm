88ca38e2298ed3d2322b28b488b7df58b3093c9b
Joel Barciauskas <jbarciauskas@bluestatedigital.com>
2013-08-30 10:58:25 -0400

    Always use /media/ephemeral0, as that is what bashton expects as well

--- recipes/cloud-generic.rb
+++ recipes/cloud-generic.rb
@@ -9,37 +9,20 @@ if(node[:ec2])
     # Unmount the Auto-mounted ephemerial drive
     # Chef's mount resource seems to balk at this, so we're scripting it
     #
-    if(node[:platform] == 'centos')
-      mount "/dev/xvdb" do
-        device "/dev/xvdb"
-        action [:umount, :disable]
-      end
-
-      bash "unmount_default_ephemeral" do
-        code <<-EOH
-          umount /mnt/ephemeral
-        EOH
-        user "root"
-        cwd "/tmp"
-        only_if "df | grep xvdb"
-      end
-    end
 
     # First device comes mounted
-    if(node[:platform] == 'amazon')
-      mount "/dev/sdb" do
-        device "/dev/sdb"
-        action [:umount, :disable]
-      end
+    mount "/dev/sdb" do
+      device "/dev/sdb"
+      action [:umount, :disable]
+    end
 
-      bash "unmount_default_ephemeral" do
-        code <<-EOH
-          umount /media/ephemeral0
-        EOH
-        user "root"
-        cwd "/tmp"
-        only_if "df | grep xvdb"
-      end
+    bash "unmount_default_ephemeral" do
+      code <<-EOH
+        umount /media/ephemeral0
+      EOH
+      user "root"
+      cwd "/tmp"
+      only_if "df | grep xvdb"
     end
 
     # First device comes mounted
