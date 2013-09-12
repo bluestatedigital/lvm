cd8cad9f45be7ca705e2dacc0467dcccd738dc74
Toby Lawrence <toby@bluestatedigital.com>
2013-09-12 13:22:07 -0400

    Add a new version of the cloud-generic recipe, called cloud-database, to test pulling the list of ephemeral drives from node attributes rather than relying on hard-coded paths.

---
+++ recipes/cloud-database.rb
@@ -0,0 +1,46 @@
+# Cookbook Name:: lvm
+# Recipe:: cloud-generic
+
+include_recipe "lvm::default"
+
+if node[:ec2]
+    # This is the maximum number of ephemeral drives any instance can allocate.
+    max_ephemeral_count = 4
+    ephemeral_drives = []
+
+    # Try and gather all of the assigned ephemeral drives.
+    max_ephemeral_count.times do |i|
+        s = "block_device_mapping_ephemeral#{i}".to_sym
+        if node[:ec2][s]
+            ephemeral_drives << "/dev/#{node[:ec2][s]}"
+        end
+    end
+
+    # Unmount the auto-mounted ephemeral drive.  Seems to be the first one, for whatever reason.
+    mount "/dev/sdb" do
+      device "/dev/sdb"
+      action [:umount, :disable]
+    end
+
+    bash "unmount_default_ephemeral" do
+      code <<-EOH
+        umount /media/ephemeral0
+      EOH
+      user "root"
+      cwd "/tmp"
+      only_if "df | grep xvdb"
+    end
+
+    # Now create our volume group based on all the ephemeral drives present.
+    lvm_volume_group 'vg0' do
+      physical_volumes ephemeral_drives
+      logical_volume 'data1' do
+        size '85%VG'
+        filesystem 'ext4'
+        mount_point :location => '/media/ephemeral0', :options => 'noatime,nodiratime'
+        stripes 4
+      end
+    end
+  end
+end
+
