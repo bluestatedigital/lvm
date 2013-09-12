7b10552a4b6f96422739687553d2fdcd5a31ea43
Toby Lawrence <toby@bluestatedigital.com>
2013-09-12 15:32:34 -0400

    Some structuring tweaks and more genericness for the lvm::cloud-database recipe.

--- recipes/cloud-database.rb
+++ recipes/cloud-database.rb
@@ -3,10 +3,11 @@
 
 include_recipe "lvm::default"
 
+ephemeral_drives = []
+
 if node[:ec2]
   # This is the maximum number of ephemeral drives any instance can allocate.
   max_ephemeral_count = 4
-  ephemeral_drives = []
 
   # Try and gather all of the assigned ephemeral drives.
   max_ephemeral_count.times do |i|
@@ -17,30 +18,38 @@ if node[:ec2]
   end
 
   if !ephemeral_drives.empty?
+    first_ed = ephemeral_drives[0]
+    first_ed_code = first_ed.gsub(/\/dev\//, '')
+
     # Unmount the auto-mounted ephemeral drive.  Seems to be the first one, for whatever reason.
-    mount "/dev/sdb" do
-      device "/dev/sdb"
+    mount first_ed do
+      device first_ed
       action [:umount, :disable]
     end
 
+    # We also try to unmount it manually because Chef doesn't like to do it via the mount resource
+    # depending on the orientation of the moon, or the mood Mitt Romney was in when he woke up this morning.
     bash "unmount_default_ephemeral" do
       code <<-EOH
         umount /media/ephemeral0
       EOH
       user "root"
       cwd "/tmp"
-      only_if "df | grep xvdb"
+      only_if "df | grep /media/ephemeral0"
     end
+  end
+end
 
-    # Now create our volume group based on all the ephemeral drives present.
-    lvm_volume_group 'vg0' do
-      physical_volumes ephemeral_drives
-      logical_volume 'data1' do
-        size '85%VG'
-        filesystem 'ext4'
-        mount_point :location => '/media/ephemeral0', :options => 'noatime,nodiratime'
-        stripes ephemeral_drives.length
-      end
+# Now assemble our VG/LV if we found any ephemeral drives.
+if !ephemeral_drives.empty?
+  # Now create our volume group based on all the ephemeral drives present.
+  lvm_volume_group 'vg0' do
+    physical_volumes ephemeral_drives
+    logical_volume 'data1' do
+      size '85%VG'
+      filesystem 'ext4'
+      mount_point :location => '/media/ephemeral0', :options => 'noatime,nodiratime'
+      stripes ephemeral_drives.length
     end
   end
 end
